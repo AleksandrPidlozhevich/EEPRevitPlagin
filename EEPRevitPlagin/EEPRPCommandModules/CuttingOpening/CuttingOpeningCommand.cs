c4acc5d7175aed5219506f165bdc2f8403373e2c
Александр Пидложевич <aleksandr.pidlozhevich@outlook.com>
2023-03-10 18:31:13 +0300

    home

--- EEPRevitPlagin/EEPRPCommandModules/CuttingOpening/CuttingOpeningCommand.cs
+++ EEPRevitPlagin/EEPRPCommandModules/CuttingOpening/CuttingOpeningCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,23 @@ using System.Threading.Tasks;
 
 namespace EEPRevitPlagin.EEPRPCommandModules.СuttingOpening
 {
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
     internal class СuttingOpeningCommand : IExternalCommand
 
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
+            // Получение текущего документа
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            //Получение доступа к Selection
+            Selection sel = commandData.Application.ActiveUIDocument.Selection;
+            Guid intersectionPointWidthGuid = new Guid("8f2e4f93-9472-4941-a65d-0ac468fd6a5d");
+            Guid intersectionPointHeightGuid = new Guid("da753fe3-ecfa-465b-9a2c-02f55d0c2ff1");
+            Guid intersectionPointThicknessGuid = new Guid("293f055d-6939-4611-87b7-9a50d0c1f50e");
+            Guid intersectionPointDiameterGuid = new Guid("9b679ab7-ea2e-49ce-90ab-0549d5aa36ff");
+            Guid heightOfBaseLevelGuid = new Guid("9f5f7e49-616e-436f-9acc-5305f34b6933");
+            Guid levelOffsetGuid = new Guid("515dc061-93ce-40e4-859a-e29224d80a10");
+
             throw new NotImplementedException();
         }
     }
